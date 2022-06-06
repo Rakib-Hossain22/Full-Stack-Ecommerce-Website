770966a867200d62e567ce9e4a946be04d8e89c4
Md Rakib Hossain <rakibuits1059@gmail.com>
2022-06-06 10:12:05 +0600

    FrontEnd React

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=100%&q=60",
+        title: "SUMMER SALE",
+        desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=100%&q=60",
+        title: "AUTUMN COLLECTION",
+        desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=100%&q=60",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=200&q=60",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=200&q=60",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=200&q=60",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/118215253/original/ea8f3cc67d36e2c1a85400c7fc990f885a172687/girls-baby-white-and-red-contrast-frock.jpg",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/550x/12/4e/12/124e12f88848fd1ae08867e28407e82e.jpg",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/736x/61/dc/65/61dc65c3cb245bf45ffd5265cc82c104.jpg",
+    },
+    {
+        id: 4,
+        img: "https://www.dhresource.com/0x0/f2/albu/g6/M01/BD/EB/rBVaR1pdjbWACFBJAAFgFNKvZBk111.jpg",
+    },
+    {
+        id: 5,
+        img: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/118215253/original/ea8f3cc67d36e2c1a85400c7fc990f885a172687/girls-baby-white-and-red-contrast-frock.jpg",
+    },
+    {
+        id: 6,
+        img: "https://i.pinimg.com/550x/12/4e/12/124e12f88848fd1ae08867e28407e82e.jpg",
+    },
+    {
+        id: 7,
+        img: "https://i.pinimg.com/736x/61/dc/65/61dc65c3cb245bf45ffd5265cc82c104.jpg",
+    },
+    {
+        id: 8,
+        img: "https://www.dhresource.com/0x0/f2/albu/g6/M01/BD/EB/rBVaR1pdjbWACFBJAAFgFNKvZBk111.jpg",
+    },
+];
